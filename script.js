9c5912f71e21c99f9f6624e98af2e1ab79ebebe3
Felipe P. <fpolidasilva@gmail.com>
2025-04-16 17:55:45 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
-function acionarBotao()
-{
-   var textoNome;
-   var textoSenha;
+function verificarLogin(){
+   let usuario = document.getElementById("txtUsuario").value.trim();
+   let senha = document.getElementById("txtSenha").value.trim();
+   let mensagemErro = document.getElementById("mensagemErro");
 
-
-   textoNome = document.getElementById('txtUsuario').value;
-
-   textoSenha = document.getElementById('txtSenha').value;
-
-
-   if(textoNome =="")
-    {
-    alert("Preencha o campo nome");
+   if(usuario =="")
+      {
+      alert("Preencha o campo nome!");
+     }
+     else if(senha=="")
+      {
+      alert("Preencha o campo senha!");
+     }
+    else {
+       if(usuario=="admin" && senha=="admin"){
+         alert("Login administrador realizado com sucesso");
+         window.location.href ="paginaAdm.html";
+      } else if(usuario=="usuario" && senha=="usuario"){
+         alert("Login realizado com sucesso");
+         window.location.href ="paginaUsuario.html";
+      }else{
+          alert("Usuário ou senha inválidos!");
+          mensagemErro.textContent ="Usuário ou senha incorretos.";
+          mensagemErro.style.color="red";
+      }
    }
-   else if(textoSenha =="")
-    {
-    alert("Preencha o campo senha");
+   }
+
+function cadastrar(){
+   let usuario = document.getElementById("txtUsuario").value.trim();
+   let senha = document.getElementById("txtSenha").value.trim();
+   let dataNascimento = document.getElementById("txtdtNascimento").value;
+   let tipoConta = document.getElementById("selTipoConta").value;
+
+   if(usuario == "" || senha == "" || dataNascimento == "" || tipoConta == ""){
+       alert("Todos os campos devem ser preenchidos!");
+       return;
+   }
+   else{
+       validarIdade(dataNascimento);
+   }
+}
+
+function validarIdade(dataNascimento){
+   let dataAtual = new Date();
+   let dataNasc = new Date(dataNascimento);
+   let idade = dataAtual.getFullYear() - dataNasc.getFullYear();
+
+   if(idade < 18){
+       alert('Você não pode estar aqui!');
+       return;
    }
    else {
-    if(textoNome == "usuarioCorreto" && textoSenha == "senhaCorreta")
-       {
-       window.location.href = 'paginaUsuario.html';
-       } 
-       else
-       {
-        alert("Usuário e/ou senha incorretos. Tente novamente");
-        return;
-       }    
+       alert('Cadastro realizado com sucesso!');
+       alert('Seja bem-vindo');
+       window.location.href="login.html"
+       
    }
-   
 }
