e0f733dda7c8e58c2fda1435d49b43909d9e5b71
Felipe P. <fpolidasilva@gmail.com>
2025-04-17 13:00:09 -0300

    Página sobre lutas, atualização e polimento do cadastro

--- script.js
+++ script.js
@@ -26,135 +26,76 @@ function verificarLogin(){
     }   
 }
 
-
 function buscarEnderecoPorCEP() {
 
-   const cep = document.getElementById("cep").value;
+    const cep = document.getElementById("cep").value;
+    if (cep.length !== 8) {
+        alert("CEP inválido. Deve conter 8 números.");
+        return;
+    }
 
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
 
-
-   if (cep.length !== 8) {
-
-       alert("CEP inválido. Deve conter 8 números.");
-
-       return;
-
-   }
-
-
-
-   const url = `https://viacep.com.br/ws/${cep}/json/`;
-
-   //const url_toda = 'viacep.com.br/ws/' + cep + '/json/';
-
-
-
-   fetch(url)
-
-       .then(response => response.json())
-
-       .then(data => {
-
-           if (data.erro) {
-
-               alert("CEP não encontrado.");
-
-               document.getElementById("endereco").value = "";
-
-           } else {
-
-               const enderecoCompleto = `${data.logradouro}`;
-
-               document.getElementById("endereco").value = enderecoCompleto;
-
-           }
-
-       })
-
-       .catch(error => {
-
-           console.error("Erro ao buscar o CEP:", error);
-
-           alert("Erro ao buscar o CEP.");
-
-       });
-
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.erro) {
+                alert("CEP não encontrado.");
+                document.getElementById("endereco").value = "";
+            } else {
+                const enderecoCompleto = `${data.logradouro}`;
+                document.getElementById("endereco").value = enderecoCompleto;
+            }
+        })
+        .catch(error => {
+            console.error("Erro ao buscar o CEP:", error);
+            alert("Erro ao buscar o CEP.");
+        });
 }
 
 function validarFormulario() {
 
-   const nome = document.getElementById("nome").value.trim();
-
+   const nome = document.getElementById("txtNome").value.trim();
+   const sobrenome = document.getElementById("txtSobrenome").value.trim();
+   const email = document.getElementById("txtEmail").value.trim();
    const dataNascimento = document.getElementById("dataNascimento").value;
-
-   const renda = document.getElementById("rendaFamiliar").value;
-
-   const endereco = document.getElementById("endereco").value;
+   const endereco = document.getElementById("txtEndereco").value;
 
    if (nome === "") {
-
        alert("Por favor, preencha o nome.");
-
        return;
-
-   }
-
-   if (!validarIdade(dataNascimento)) {
-
-       alert("Você deve ser maior de idade para se cadastrar.");
-
-       return;
-
-   }
-
-   if (!validarRenda(renda)) {
-
-       alert("A renda familiar deve ser um número positivo.");
-
-       return;
-
-   }
-
-   // Verificação se endereço foi preenchido
-
-   if (endereco === "") {
-
-       alert("Preencha um CEP válido para obter o endereço.");
-
-       return;
-
-   }
-
-   alert("Formulário enviado com sucesso!");
-
 }
-
-// Verifica se a pessoa tem 18 anos ou mais
+   if (sobrenome === "") {
+        alert("Por favor, preencha o sobrenome.");
+        return;
+}
+    if (email === "") {
+        alert("Por favor, preencha o email.");
+         return;
+}
+   if (!validarIdade(dataNascimento)) {
+       alert("Você deve ser maior de idade para se cadastrar.");
+       return;
+}
+   if (endereco === "") {
+       alert("Preencha um CEP válido para obter o endereço.");
+       return;
+   }
+   
+     alert("Cadastro realizado com sucesso!");
+     window.location.href="login.html";
+}
 
 function validarIdade(dataNascimento) {
 
    const hoje = new Date();
-
    const nascimento = new Date(dataNascimento);
-
    let idade = hoje.getFullYear() - nascimento.getFullYear();
-
    const mes = hoje.getMonth() - nascimento.getMonth();
 
    if (mes < 0 || (mes === 0 && hoje.getDate() < nascimento.getDate())) {
-
        idade--;
-
    }
-
    return idade >= 18;
-
-}
-
-function validarRenda(valor) {
-
-   const renda = parseFloat(valor);
-
-   return !isNaN(renda) && renda > 0;
 
 }
